26b8c590e4175cfd5ead249d135ffe2e35924aff
Admin <Admin@PARAD>
2022-05-30 16:00:13 +0700

    Add project files.

---
+++ BigSchool_DoHoangLongVu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_DoHoangLongVu.Startup))]
+namespace BigSchool_DoHoangLongVu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
